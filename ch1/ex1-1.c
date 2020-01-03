c71e43263c959afe406e954e041f52ddcf5596ae
Kee Ryun Park <keeryun.park@gmail.com>
2020-01-03 00:07:19 -0800

    Added answers for exercise 1.1, 1.2

---
+++ ch1/ex1-1.c
@@ -0,0 +1,12 @@
+/*  Exercise 1-1:
+ *  Run the "hello, wrold" program on your system. Experiment without leaving
+ *  out parts of the program, to see what error messages you get. 
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("hello, world\n");
+    return 0;
+}
